2a787c5df1310bec55c658accda5712bca261d52
kdeluz <kdeluz@rrc.ca>
2024-05-08 11:56:25 -0500

    Completed Part 3 of User Input

--- Challenge 0/challenge00.rb
+++ Challenge 0/challenge00.rb
@@ -2,16 +2,18 @@
 # Challenge 00
 # A New Beginning
 
-# Part 1: Write a Ruby program to calculate the after tax value of a specific dollar amount. 
-sub_total = 30.00
+# Part 1: Write a Ruby program to calculate the after tax value of a specific dollar amount.
+# Part 3: Includes a way to insert dollar amounts from the user
+print "Enter a dollar amount: $" 
+sub_total = gets.to_f
 PST = 0.07
 GST = 0.05
 
 grand_total = sub_total * (1+(PST + GST))
 
-puts "Subtotal: $#{sub_total}"
-puts "PST: $#{PST}"
-puts "GST: $#{GST}"
+puts "Subtotal: $#{sub_total.round(3)}"
+puts "PST: $#{sub_total.round(3) * PST}"
+puts "GST: $#{sub_total.round(3) * GST}"
 puts "$#{grand_total.round(3)}"
 
 # Part 2: Update your Ruby program from question one such that it prints a short message after the grand total.
@@ -22,3 +24,4 @@ elsif grand_total > 5.0 && grand_total < 20.0
 elsif grand_total >= 20.0
     puts "Charge It!"
 end
+
